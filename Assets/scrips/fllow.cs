6770e0e9a1619920746f488dc4aa8f93483cd4c1
yan <1060943199@qq.com>
2022-04-22 14:52:49 +0800

    unity3D

---
+++ Assets/scrips/fllow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fllow : MonoBehaviour { 
+    public Transform playerTransfrom;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset=transform.position - playerTransfrom.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerTransfrom.position + offset;
+    }
+}
